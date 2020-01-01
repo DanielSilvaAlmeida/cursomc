f769efeeeebd598919637b7538ae9e293219ba8c
DanielSilvaAlmeida <daniel.atm.silva@gmail.com>
2020-01-01 10:51:05 -0200

    Teste da primeira classe de domínio Categoria

--- src/main/java/com/daniel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/daniel/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.daniel.cursomc.resources;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.cursomc.domain.Categoria;
 
 // Anotação para aplicações REST
 @RestController
@@ -9,9 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	// Define que o méetodo terá a função de GET
+	// Define que o método terá a função de GET
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
