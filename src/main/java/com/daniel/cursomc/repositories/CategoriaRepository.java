b274e351553af49496feabc913646add2ede82f5
DanielSilvaAlmeida <daniel.atm.silva@gmail.com>
2020-01-10 09:35:41 -0200

    Criação de camada REST e de SERVICO

---
+++ src/main/java/com/daniel/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.daniel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daniel.cursomc.domain.Categoria;
+
+// Interface responsável pela camada de acesso a dados
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
